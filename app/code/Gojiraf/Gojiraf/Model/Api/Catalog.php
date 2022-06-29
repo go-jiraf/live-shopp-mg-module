9cb5188e12e8fef3f559c138b3d20f1ebc38465a
Nicolas Oleinizak <nicolas.oleinizak@gmail.com>
2022-06-29 13:12:18 -0300

    feat: add multisource stock compatibility

--- app/code/Gojiraf/Gojiraf/Model/Api/Catalog.php
+++ app/code/Gojiraf/Gojiraf/Model/Api/Catalog.php
@@ -1,43 +1,50 @@
 <?php 
 
 namespace Gojiraf\Gojiraf\Model\Api;
-//use Magento\Framework\Exception\InputException;
+use Zend_Db_Expr;
 
-
-class Catalog{
+class Catalog
+{
     private $variantAttributes;
     private $imageHelper;
+    private $isDefaultStock;
 
     protected $productCollectionFactory;
     protected $productVisibility;
     protected $productStatus;
+
+    protected $objectManager;
 
     public $catalogVersion = "V.1.4";
     public function getCatalogVersion(){
         return $this->catalogVersion;
     }
     // /rest/V1/gojiraf/productlist/page/1?searchTerm=Camisa&limit=10&ids=23,31
-    public function getProductList($page = 1, $limit = 10, $searchTerm = NULL, $ids = ""){
+    public function getProductList($page = 1, $limit = 10, $searchTerm = NULL, $ids = "")
+    {
 
-        $this->imageHelper = \Magento\Framework\App\ObjectManager::getInstance()->get('\Magento\Catalog\Helper\Image');
-        $this->stockRegistry = \Magento\Framework\App\ObjectManager::getInstance()->get('\Magento\CatalogInventory\Api\StockRegistryInterface');
-        $this->productCollectionFactory = \Magento\Framework\App\ObjectManager::getInstance()->get('\Magento\Catalog\Model\ResourceModel\Product\CollectionFactory');
-        $this->productStatus = \Magento\Framework\App\ObjectManager::getInstance()->get('\Magento\Catalog\Model\Product\Attribute\Source\Status');
-        $this->productVisibility = \Magento\Framework\App\ObjectManager::getInstance()->get('\Magento\Catalog\Model\Product\Visibility');
+        $this->objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+
+        $this->imageHelper = $this->objectManager->get('\Magento\Catalog\Helper\Image');
+        $this->stockRegistry = $this->objectManager->get('\Magento\CatalogInventory\Api\StockRegistryInterface');
+        $this->productCollectionFactory = $this->objectManager->get('\Magento\Catalog\Model\ResourceModel\Product\CollectionFactory');
+        $this->productStatus = $this->objectManager->get('\Magento\Catalog\Model\Product\Attribute\Source\Status');
+        $this->productVisibility = $this->objectManager->get('\Magento\Catalog\Model\Product\Visibility');
+        
+        $this->isDefaultStock = $this->isDefaultStock();
 
         $productCollection = $this->prepareCollection();
         $filteredCollection = $this->filterCollection($productCollection, $page, $limit, $searchTerm, $ids);
 
         if (empty($filteredCollection->getData())){
             return [];
-            //throw new InputException(__("No se encontraron productos."));
         }
 
         $productList = array();
-        foreach ($filteredCollection as $productModel){
+        foreach ($filteredCollection as $productModel) {
             if ($productModel->getTypeId() == 'configurable') {
                 $productData = $this->buildConfigProductData($productModel);
-            }else{
+            } else { 
                 $productData = $this->buildSimpleProductData($productModel);
             }
             if (!empty($productData)) {
@@ -47,18 +54,38 @@ class Catalog{
         return $productList;
     }
 
-    public function prepareCollection(){
+    private function prepareCollection()
+    {
         $productCollection = $this->productCollectionFactory->create();
         $productCollection->addAttributeToSelect('*');
         $productCollection->addAttributeToFilter('status', ['in' => $this->productStatus->getVisibleStatusIds()]);
         $productCollection->setVisibility($this->productVisibility->getVisibleInSiteIds());
 
+        if($this->isDefaultStock){
+            $productCollection->setFlag('has_stock_status_filter', false);
+            $productCollection->joinField(
+                'stock_item', 
+                'cataloginventory_stock_item', 
+                'is_in_stock', 
+                'product_id=entity_id', 
+                'is_in_stock=1'
+            );
+        } else {
+            $getStockIdForCurrentWebsite = $this->objectManager->get('Magento\InventoryCatalog\Model\GetStockIdForCurrentWebsite');
+            $stockId = $getStockIdForCurrentWebsite->execute();
+            $productCollection
+            ->getSelect()
+            ->join(
+                array('stock_item' => new Zend_Db_Expr("( SELECT sku, quantity, is_salable FROM inventory_stock_"."$stockId"." WHERE is_salable = true AND quantity > 0)")),
+                'e.sku = stock_item.sku',
+                array('')
+            );
+        }
         return $productCollection;
-
     }
 
-
-    public function filterCollection($productCollection, $page, $limit, $searchTerm, $ids){
+    private function filterCollection($productCollection, $page, $limit, $searchTerm, $ids)
+    {
         $offset = ($page == 0 || $page == 1) ? 0 : $page * $limit - 1;
 
         // Si pide IDs de productos especificos, los filtramos.
@@ -68,126 +95,170 @@ class Catalog{
         if (!empty($searchTerm) && $searchTerm != "undefined") {
             $productCollection->addAttributeToFilter('name', array('like' => "%" .$searchTerm. "%"));
         }
+
         $productCollection->getSelect()
             ->limit($limit, $offset);
 
         return $productCollection;
     }
 
+    private function buildSimpleProductData($productModel)
+    {
+        $productArray = array(
+            "id" => $productModel->getId() ,
+            "sku" => $productModel->getSku() ,
+            "description" => $productModel->getName() ,
+            "price" => "",
+            "originalPrice" => "",
+            "imageUrl" => "",
+            "stock" => $this->getStock($productModel)
+        );
 
-    public function buildSimpleProductData($productModel){
-            $stockStatus = $this->stockRegistry->getStockItem($productModel->getId());              
-            if ($stockStatus->getData('is_in_stock') == 0 || $stockStatus->getQty() == 0) {
-                return array();
-            }
-            $productArray = array(
-                "id" => $productModel->getId() ,
-                "sku" => $productModel->getSku() ,
-                "description" => $productModel->getName() ,
-                "price" => "",
-                "originalPrice" => "",
-                "imageUrl" => ""
-            );
-
-            $productArray["price"] =   (float)number_format($productModel->getFinalPrice() , 2, ",", "");
-            $productArray["originalPrice"] =   (float)number_format($productModel->getPriceInfo()->getPrice('regular_price')->getValue() , 2, ",", "");
-            $productArray["imageUrl"] = $this->getProductImage($productModel) ;
-            return $productArray;
-
+        $productArray["price"] = (float)number_format($productModel->getFinalPrice() , 2, ",", "");
+        $productArray["originalPrice"] = (float)number_format($productModel->getPriceInfo()->getPrice('regular_price')->getValue() , 2, ",", "");
+        $productArray["imageUrl"] = $this->getProductImage($productModel);
+        return $productArray;
     }
-    public function buildConfigProductData($productModel){
-            $productArray = array(
-                "id" => $productModel->getId() ,
-                "sku" => $productModel->getSku() ,
-                "description" => $productModel->getName() ,
-                "variants" => array() ,
-                "variantOptions" => array() ,
-                "price" => "",
-                "imageUrl" => ""
-            );
-
-
-            $this->variantAttributes = $productModel->getTypeInstance()
-                ->getUsedProductAttributes($productModel);
-
-            $highestPrice = 0;
-            $variantsArray = array();
-            $optionsArray = array();
-            $childProducts = $productModel->getTypeInstance()
-                ->getUsedProducts($productModel);
-            foreach ($childProducts as $child)
-            {
-                //Si la variante no tiene stock, la ignoramos.
-                $stockStatus = $this->stockRegistry->getStockItem($child->getId());
-
-                if ($stockStatus->getData('is_in_stock') == 0 || $stockStatus->getQty() == 0) {
+    
+    private function buildConfigProductData($productModel)
+    {
+        $productArray = array(
+            "id" => $productModel->getId() ,
+            "sku" => $productModel->getSku() ,
+            "description" => $productModel->getName() ,
+            "variants" => array() ,
+            "variantOptions" => array() ,
+            "price" => "",
+            "imageUrl" => ""
+        );
+        
+        $this->variantAttributes = $productModel->getTypeInstance()
+        ->getUsedProductAttributes($productModel);
+        
+        $highestPrice = 0;
+        $variantsArray = array();
+        $optionsArray = array();
+        $childProducts = $productModel->getTypeInstance()
+            ->getUsedProducts($productModel);
+        foreach ($childProducts as $child)
+        {
+            //Si la variante no tiene stock, la ignoramos.
+            $stockStatus = $this->stockRegistry->getStockItem($child->getId());
+            
+            if($this->isDefaultStock){
+                if($stockStatus->getData('is_in_stock') == 0 || $stockStatus->getQty() == 0){
                     continue;
                 }
-                //Acomodamos datos de las posibles variantes
-                $option = array();
-                foreach ($this->variantAttributes as $attribute)
+            } else {
+                if($this->getStock($child) == 0){
+                    continue;
+                }
+            }
+
+            //Acomodamos datos de las posibles variantes
+            $option = array();
+            foreach ($this->variantAttributes as $attribute)
+            {
+                $attributeValue = $child->getResource()
+                ->getAttribute($attribute->getAttributeCode())
+                ->getFrontend()
+                ->getValue($child);
+                array_push($option, $attributeValue);
+                if (!isset($variantsArray[$attribute->getFrontendLabel() ]))
                 {
-                    $attributeValue = $child->getResource()
-                        ->getAttribute($attribute->getAttributeCode())
-                        ->getFrontend()
-                        ->getValue($child);
-                    array_push($option, $attributeValue);
-                    if (!isset($variantsArray[$attribute->getFrontendLabel() ]))
+                    $variantsArray[$attribute->getFrontendLabel() ] = array();
+                    array_push($variantsArray[$attribute->getFrontendLabel() ], $attributeValue);
+                }
+                else
+                {
+                    if (!in_array($attributeValue, $variantsArray[$attribute->getFrontendLabel() ]))
                     {
-                        $variantsArray[$attribute->getFrontendLabel() ] = array();
                         array_push($variantsArray[$attribute->getFrontendLabel() ], $attributeValue);
                     }
-                    else
-                    {
-                        if (!in_array($attributeValue, $variantsArray[$attribute->getFrontendLabel() ]))
-                        {
-                            array_push($variantsArray[$attribute->getFrontendLabel() ], $attributeValue);
-                        }
-                    }
                 }
-                $imageUrl = $this->getProductImage($child);
-                $childPrice = (float)number_format($child->getFinalPrice() , 2, ",", ""); 
-                $childOriginalPrice = (float)number_format($child->getPriceInfo()->getPrice('regular_price')->getValue() , 2, ",", "");
-                if ($childOriginalPrice > $highestPrice) {
-                    $highestPrice = $childOriginalPrice;
-                }
-                //Acomodamos datos del producto simple
-                array_push($optionsArray, array(
-                    "option" => $option,
-                    "sku" => $child->getSku() ,
-                    "price" => $childPrice ,
-                    "imageUrl" => $imageUrl,
-                    "originalPrice" => $childOriginalPrice,
-                    "description" => $child->getName()
-                ));
             }
+            $imageUrl = $this->getProductImage($child);
+            $childPrice = (float)number_format($child->getFinalPrice() , 2, ",", ""); 
+            $childOriginalPrice = (float)number_format($child->getPriceInfo()->getPrice('regular_price')->getValue() , 2, ",", "");
+            if ($childOriginalPrice > $highestPrice) {
+                $highestPrice = $childOriginalPrice;
+            }
+            //Acomodamos datos del producto simple
+            array_push($optionsArray, array(
+                "option" => $option,
+                "sku" => $child->getSku() ,
+                "price" => $childPrice ,
+                "imageUrl" => $imageUrl,
+                "originalPrice" => $childOriginalPrice,
+                "description" => $child->getName(),
+                "stock" => $this->getStock($child)
+            ));
+        }
 
-            foreach ($variantsArray as $key => $variants)
-            {
-                array_push($productArray["variants"], array(
-                    "name" => $key,
-                    "options" => $variants
-                ));
-            }
+        foreach ($variantsArray as $key => $variants)
+        {
+            array_push($productArray["variants"], array(
+                "name" => $key,
+                "options" => $variants
+            ));
+        }
+        
+        //aca las variantOptions
+        if (empty($optionsArray)) {
+            return array();
+        }
+        $productArray["variantOptions"] = $optionsArray;
+        $configProductPrice = (float)number_format($child->getPriceInfo()->getPrice('regular_price')->getValue() , 2, ",", "");
+        $productArray["price"] =  ($configProductPrice == 0 ) ? $highestPrice : $configProductPrice ;
+        $productArray["imageUrl"] = $this->getProductImage($productModel);
 
-            //aca las variantOptions
-            if (empty($optionsArray)) {
-                return array();
-            }
-            $productArray["variantOptions"] = $optionsArray;
-            $configProductPrice = (float)number_format($child->getPriceInfo()->getPrice('regular_price')->getValue() , 2, ",", "");
-            $productArray["price"] =  ($configProductPrice == 0 ) ? $highestPrice : $configProductPrice ;
-            $productArray["imageUrl"] = $this->getProductImage($productModel) ;
-            return $productArray;
+        return $productArray;
     }
-
-
-    public function getProductImage($product)
+    
+    private function getProductImage($product)
     {
         $imageUrl = $this
-            ->imageHelper
-            ->init($product, 'product_page_image')->setImageFile($product->getImage()) // image,small_image,thumbnail
-            ->getUrl();
+        ->imageHelper
+        ->init($product, 'product_page_image')->setImageFile($product->getImage()) // image,small_image,thumbnail
+        ->getUrl();
         return $imageUrl;
+    }
+    
+    private function isDefaultStock()
+    {
+        $productMetadata = $this->objectManager->get('\Magento\Framework\App\ProductMetadataInterface');
+        $magentoVersion = $productMetadata->getVersion();
+        if(version_compare($magentoVersion, "2.3", '<')){
+            return true;
+        }
+        
+        $getStockIdForCurrentWebsite = $this->objectManager->get('Magento\InventoryCatalog\Model\GetStockIdForCurrentWebsite');
+        $stockId = $getStockIdForCurrentWebsite->execute();
+        $getSources = $this->objectManager->get('Magento\Inventory\Model\Source\Command\GetSourcesAssignedToStockOrderedByPriority');
+        $sources = $getSources->execute($stockId);
+
+        // If there are more than 1 source on active stock, is multisource
+        if(count($sources) > 1){
+            return false;
+        }
+        // If the active stock is not default, then is multisource
+        if($sources[0]->getSourceCode() != 'default'){
+            return false;
+        }
+
+        return true;
+    }
+    
+    private function getStock($productModel)
+    {
+        if($this->isDefaultStock){
+            return $this->stockRegistry->getStockItem($productModel->getId())->getQty();
+        } else {
+            $getStockIdForCurrentWebsite = $this->objectManager->get('Magento\InventoryCatalog\Model\GetStockIdForCurrentWebsite');
+            $stockId = $getStockIdForCurrentWebsite->execute();
+            $getProductSalableQty = $this->objectManager->get('Magento\InventorySales\Model\GetProductSalableQty');
+            $qty = $getProductSalableQty->execute($productModel->getSku(), $stockId);
+            return $qty;
+        }
     }
 }
