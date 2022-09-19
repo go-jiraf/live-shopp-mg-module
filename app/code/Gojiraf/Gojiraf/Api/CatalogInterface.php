e4e3c9c8a628058412f75b91ccb441c97f948383
Nicolas Oleinizak <nicolas.oleinizak@gmail.com>
2022-09-19 08:19:32 -0300

    add filterByStock option flag

--- app/code/Gojiraf/Gojiraf/Api/CatalogInterface.php
+++ app/code/Gojiraf/Gojiraf/Api/CatalogInterface.php
@@ -10,8 +10,9 @@ interface CatalogInterface
      * @param string $limit
      * @param string $searchTerm
      * @param mixed[] $ids
+     * @param bool $filterByStock
      * @return string
      */
  
-    public function getProductList($page, $limit = 10, $searchTerm = NULL, $ids = "");
+    public function getProductList($page, $limit = 10, $searchTerm = NULL, $ids = "", $filterByStock = true);
 }
