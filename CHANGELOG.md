2dbe4a0bcdf18917eec2b5a08d79ffe50e9d5dfd
nicolasoleinizak-gj <nicolas.oleinizak@gojiraf.com>
2023-09-26 19:34:36 -0300

    refactor: improve compatibility for configurable products

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,17 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
+## [1.3.0] 2023-09-29
+
+### Feature
+
+This new minor version presents the GoJiraf webhook feature, that sends a request to GoJiraf Services each time a order is paid,
+in order to update the Gojiraf order with the proper status, improving the business analytics.
+
+- Create a Webhooks Configuration table in database
+- Add a REST API and custom resource to get, create and delete webhook configurations for Gojiraf
+- Add a Observer for the sales_order_payment_pay event
+
 ## [1.2.4] 2023-10-18
 
 ### Fix
@@ -16,6 +27,12 @@ and this project adheres to [Semantic Versioning](http://semver.org/).
 ### Fix
 
 - Downgrade the PHP Version requirement (7.3 or higher, instead of 8.0/8.1)
+## [1.2.2] 2023-09-26
+
+### Refactor
+
+- Improve Redirect class for a more standar behavior when adding configurable products to the cart
+- General style refactor in Redirect class
 
 ## [1.2.0] 2023-03-23
 
--- Controller/Cart/Redirect.php
+++ Controller/Cart/Redirect.php
@@ -2,47 +2,60 @@
 namespace Gojiraf\Gojiraf\Controller\Cart;
 
 use \Magento\Framework\App\Action\Context;
+use \Magento\Checkout\Model\Session;
+use \Magento\Checkout\Model\Cart;
+use \Magento\Catalog\Model\ProductRepository;
+use \Magento\Quote\Api\CartRepositoryInterface;
+use \Magento\Quote\Model\GuestCart\GuestCartRepository;
+use \Magento\Framework\Message\ManagerInterface;
+use \Magento\ConfigurableProduct\Model\Product\Type\Configurable;
+use \Magento\Framework\Controller\Result\JsonFactory;
 
 class Redirect extends \Magento\Framework\App\Action\Action
 {
     protected $_checkoutSession;
     protected $_cart;
     protected $_quote;
-    protected $_productloader;
+    protected $_productRepository;
     protected $_cartRepository;
     protected $_guestCartRepository;
     protected $_messageInterface;
+    protected $_configurable;
+    protected $_jsonFactory;
+
     public function __construct(Context $context,
-                                \Magento\Checkout\Model\Session $checkoutSession,
-                                \Magento\Checkout\Model\Cart $cart,
-                                \Magento\Catalog\Model\ProductFactory $productloader,
-                                \Magento\Quote\Api\CartRepositoryInterface $cartRepository,
-                                \Magento\Customer\Model\Session $customerSession,
-                                \Magento\Quote\Model\GuestCart\GuestCartRepository $guestCartRepository,
-                                \Magento\Framework\Message\ManagerInterface $messageInterface
-                            )
+        Session $checkoutSession,
+        Cart $cart,
+        ProductRepository $productRepository,
+        CartRepositoryInterface $cartRepository,
+        GuestCartRepository $guestCartRepository,
+        ManagerInterface $messageInterface,
+        Configurable $configurable,
+        JsonFactory $jsonFactory,
+    )
     {
         parent::__construct($context);
         $this->_checkoutSession = $checkoutSession;
         $this->_cart = $cart;
         $this->_cartRepository = $cartRepository;
-        $this->_productloader = $productloader;
+        $this->_productRepository = $productRepository;
         $this->_guestCartRepository = $guestCartRepository;
         $this->_messageInterface = $messageInterface;
+        $this->_configurable = $configurable;
+        $this->_jsonFactory = $jsonFactory;
     }
 
     public function execute()
     {
         $params = $this->getRequest()->getParams();
         $cartId = $params["CART_ID"];
-        $formKey = \Magento\Framework\App\ObjectManager::getInstance()->get('Magento\Framework\Data\Form\FormKey');
 
         $this->_quote = $this->_checkoutSession->getQuote();
         if (empty($cartId)) {
             $this->getResponse()->setStatusCode(\Magento\Framework\App\Response\Http::STATUS_CODE_503);
             return $this->displayError("EMPTY_CART_ID","Ocurrió un error al asociar tu carrito de compras.");
         }
-        
+
         try {
             $guestCart = $this->_guestCartRepository->get($cartId);
         } catch (\Magento\Framework\Exception\NoSuchEntityException $e) {
@@ -51,20 +64,10 @@ class Redirect extends \Magento\Framework\App\Action\Action
         }
 
         foreach ($guestCart->getItems() as $item) {
-            $request = new \Magento\Framework\DataObject();
-            $params = array(
-                        'form_key' => $formKey,
-                        'product' => $item->getProductId(), 
-                        'qty'   => $item->getQty()              
-                    ); 
-            $request->setData($params);
-
-             
-            $product = $this->_productloader->create()->load($item->getProductId());
-            if ($product->isSaleable()) {
-               $this->_cart->addProduct($product, $request);
-            }else{
-                $this->_messageInterface->addNotice("El producto {$item->getName()} no tiene stock.");
+            try {
+                $this->addProductToCart($item);
+            } catch (\Exception $e) {
+                $this->displayError("ADD_PRODUCT", "Ocurrió un error al intentar agregar uno de los productos al carrito.");
             }
         }
         $this->_cart->save();
@@ -74,18 +77,46 @@ class Redirect extends \Magento\Framework\App\Action\Action
         $this->_cart->getQuote()->setTotalsCollectedFlag(false)->collectTotals()->save();
         $this->redirectSuccess();
     }
+
+    private function addProductToCart ($item) {
+        $productId = $item->getProductId();
+        $product = $this->_productRepository->getById($productId);
+
+        $params = array(
+                    'product' => $item->getProductId(),
+                    'qty' => $item->getQty(),
+                );
+        
+        $parentProductIds = $this->_configurable->getParentIdsByChild($productId);
+
+        if (isset($parentProductIds[0])) {
+            $parentProductId = $parentProductIds[0];
+            $params['product'] = $parentProductId;
+
+            $options = [];
+            $parentProduct = $this->_productRepository->getById($parentProductId);
+            $productAttributeOptions = $parentProduct->getTypeInstance(true)->getConfigurableAttributesAsArray($parentProduct);
+            foreach ($productAttributeOptions as $option) {
+                $options[$option['attribute_id']] = $product->getData($option['attribute_code']);
+            }
+            $params['super_attribute'] = $options;
+        }
+
+        if ($product->isSaleable()) {
+            $this->_cart->addProduct($product, $params);
+        } else {
+            $this->_messageInterface->addNotice("El producto {$item->getName()} no tiene stock.");
+        }
+    }
     
     public function redirectSuccess(){
         $this->getResponse()->setRedirect('/checkout/cart/');
     }
 
     public function displayError($errorCode, $message){
-        $jsonFactory = \Magento\Framework\App\ObjectManager::getInstance()->get('\Magento\Framework\Controller\Result\JsonFactory');
         $data = ['error_code' => $errorCode, 'message' => $message];
-        $result = $jsonFactory->create();
+        $result = $this->_jsonFactory->create();
         $response = $result->setData($data);
         return $response;
     }
-
-
 }
